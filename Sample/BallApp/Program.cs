a6044e9dd10bf24cc74cbc3e390eae2b126ac368
櫻井 優奈 <OIBC34100@ojs.ac.jp>
2023-04-25 10:39:42 +0900

    テニスボールが動いた（完成していない）

--- Sample/BallApp/Program.cs
+++ Sample/BallApp/Program.cs
@@ -11,11 +11,17 @@ namespace BallApp {
 
         private Timer moveTimer;    //タイマー用
         private SoccerBall soccerBall;
+        private TennisBall tennisBall;
         private PictureBox pb;
-        int count = 0;
-            
-        private List<SoccerBall> balls = new List<SoccerBall>();    //ボールインスタンス格納用
+
+
+
+        private List<Obj> balls = new List<Obj>();    //ボールインスタンス格納用
         private List<PictureBox> pbs = new List<PictureBox>();      //表示用
+        
+
+
+        private int count = 0;
 
         static void Main(string[] args) {
             Application.Run(new Program());
@@ -25,28 +31,53 @@ namespace BallApp {
             this.Size = new Size(800, 600);
             this.BackColor = Color.Green;
             this.MouseClick += Program_MouseClick;
+      
+
+            
 
             moveTimer = new Timer();
             moveTimer.Interval = 10; //タイマーのインターバル（ms）
             moveTimer.Tick += MoveTimer_Tick;  //デリゲート登録
         }
 
+       
+
+       
         //マウスクリック時のイベントハンドラ
         private void Program_MouseClick(object sender, MouseEventArgs e) {
-            //ボールインスタンス生成
-            soccerBall = new SoccerBall(e.X - 25, e.Y - 25);
-            pb = new PictureBox();   //画像を表示するコントロール
-            pb.Image = soccerBall.Image;
-            pb.Location = new Point((int)soccerBall.PosX, (int)soccerBall.PosY);　//画像の位置
-            pb.Size = new Size(50, 50); //画像の表示サイズ
-            pb.SizeMode = PictureBoxSizeMode.StretchImage;  //画像の表示モード
-            pb.Parent = this;
 
-            count += 1;
-            this.Text = count + "個";
+            
 
-            balls.Add(soccerBall);  
-            pbs.Add(pb);            
+            if (e.Button == MouseButtons.Left){
+                //ボールインスタンス生成
+                soccerBall = new SoccerBall(e.X - 25, e.Y - 25);
+                pb = new PictureBox();   //画像を表示するコントロール
+                pb.Image = soccerBall.Image;
+                pb.Location = new Point((int)soccerBall.PosX, (int)soccerBall.PosY);　//画像の位置
+                pb.Size = new Size(50, 50); //画像の表示サイズ
+                pb.SizeMode = PictureBoxSizeMode.StretchImage;  //画像の表示モード
+                pb.Parent = this;
+
+                //count += 1;
+                //this.Text = count + "個";
+                //this.Text = "BallGame:"  +bolls.Count;
+
+                balls.Add(soccerBall);  
+                pbs.Add(pb);
+            }else{
+                tennisBall = new TennisBall(e.X - 25, e.Y - 25);
+                pb = new PictureBox();
+                pb.Image = tennisBall.Image;
+                pb.Location = new Point((int)tennisBall.PosX, (int)tennisBall.PosY);　//画像の位置
+                pb.Size = new Size(25, 25); //画像の表示サイズ
+                pb.SizeMode = PictureBoxSizeMode.StretchImage;  //画像の表示モード
+                pb.Parent = this;
+                balls.Add(soccerBall);
+                pbs.Add(pb);
+            }
+            
+
+            this.Text = "BallGamr:" + SoccerBall.Count + ":" + TennisBall.Count;
 
             moveTimer.Start();  //タイマースタート
         }
--- Sample/BallApp/TennisBall.cs
+++ Sample/BallApp/TennisBall.cs
@@ -1,26 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace BallApp {
-    class SoccerBall : Obj{
-        //フィールド        
-        private static int count;//インスタンスの個数
+    class TennisBall : Obj {
+        //フィールド
+        private static int count;
 
         Random random = new Random();   //乱数インスタンス
 
 
         //プロパティ
-       
-        public static int Count { get => count; set => count = value;}
-
+        public static int Count { get => count; set => count = value; }
 
         //コンストラクタ
-        public SoccerBall( double xp, double yp ) 
-            : base(xp,yp, @"pic\soccer_ball.png"){
+        public TennisBall(double xp,double yp)
+            : base(xp, yp, @"pic\tennis_ball.png") {
 
             int rndX = random.Next(-25, 25);
             MoveX = (rndX != 0 ? rndX : 1); //乱数で移動量を設定
@@ -29,18 +26,23 @@ namespace BallApp {
             MoveY = (rndY != 0 ? rndY : 1); //乱数で移動量を設定
 
             Count++;
-        }       
-        
-        
+        }
+
+       
+
+
+
         //メソッド
         public override void Move() {
             //Console.WriteLine("Ｘ座標 = {0}, Ｙ座標 = {1}", PosX, PosY);
 
-            if (PosY > 520 || PosY < 0){
+            if (PosY > 520 || PosY < 0)
+            {
                 MoveY = -MoveY;
             }
 
-            if (PosX > 730 || PosX < 0){
+            if (PosX > 730 || PosX < 0)
+            {
                 MoveX = -MoveX;
             }
 
