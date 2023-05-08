5a2623d5fc332991919622604383978cc4a9e410
櫻井 優奈 <OIBC34100@ojs.ac.jp>
2023-05-08 10:54:02 +0900

    P26 まで

---
+++ Chapter01/Product/Product.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductSample {
+    //商品クラス
+    class Product {
+        //自動実装プロパティ
+        public int Code { get; set; }   //商品コード
+        public string Name { get; set; }   //商品名
+        public int Price { get; set; }  //商品価格(税抜き)
+
+
+        //コンストラクタ
+        public Product(int code,string name,int price) {
+            this.Code = code;
+            this.Name = name;
+            this.Price = price;
+        }
+
+
+        //消費税額を決める(消費税率は10％)
+        public int GetTax() {
+            return (int)(Price * 0.1);
+        }
+
+
+        //税込価格を求める
+        public int GetPriceIncludingtax() {
+            return Price + GetTax();
+        }
+    }
+}
---
+++ Chapter01/Product/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductSample {
+
+    class Program {
+        static void Main(string[] args) {
+            //インスタンスの生成（インスタンス = オブジェクト）
+            Product karintou = new Product(123,"かりんとう",180);
+            Product daihuku = new Product(235, "大福もち", 160);
+
+            Console.WriteLine("かりんとうの税込価格：" + karintou.GetPriceIncludingtax() + "円");
+            Console.WriteLine("大福もちの税込価格：" + daihuku.GetPriceIncludingtax() + "円");
+        }
+    }
+}
