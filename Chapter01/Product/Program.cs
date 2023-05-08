b6f48878044e233330098a7c9c7e91d49aacc179
櫻井 優奈 <OIBC34100@ojs.ac.jp>
2023-05-08 11:41:33 +0900

    0508演習1

--- Chapter01/Product/Program.cs
+++ Chapter01/Product/Program.cs
@@ -8,12 +8,28 @@ namespace ProductSample {
 
     class Program {
         static void Main(string[] args) {
-            //インスタンスの生成（インスタンス = オブジェクト）
-            Product karintou = new Product(123,"かりんとう",180);
-            Product daihuku = new Product(235, "大福もち", 160);
+            #region　P26のサンプルプログラム
+            ////インスタンスの生成（インスタンス = オブジェクト）
+            //Product karintou = new Product(123,"かりんとう",180);
+            //Product daihuku = new Product(235, "大福もち", 160);
 
-            Console.WriteLine("かりんとうの税込価格：" + karintou.GetPriceIncludingtax() + "円");
-            Console.WriteLine("大福もちの税込価格：" + daihuku.GetPriceIncludingtax() + "円");
+            //Console.WriteLine("かりんとうの税込価格：" + karintou.GetPriceIncludingtax() + "円");
+            //Console.WriteLine("大福もちの税込価格：" + daihuku.GetPriceIncludingtax() + "円");
+            #endregion
+
+
+            DateTime date = new DateTime(2023,5,8);
+            DateTime today = DateTime.Today;
+
+
+            //10日後を求める
+            DateTime daysAfter10 = date.AddDays(10);
+            DateTime daysBefore10 = date.AddDays(-10);
+
+            Console.WriteLine("今日の日付：" + today);
+            Console.WriteLine("１０日後　：" + daysAfter10);
+            Console.WriteLine("１０日前　：" + daysBefore10);
+
         }
     }
 }
