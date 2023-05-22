17ac58b36953d189b1b9dedb7fcdc5fa8fe723f3
櫻井 優奈 <OIBC34100@ojs.ac.jp>
2023-05-22 09:35:44 +0900

    P79chapter02　演習03　新規作成

--- Chapter02/売上集計プログラム/SalesCalculator/Program.cs
+++ Chapter02/売上集計プログラム/SalesCalculator/Program.cs
@@ -8,5 +8,15 @@ namespace SalesCalculator {
     class Program {
         static void Main(string[] args) {
         }
+
+
+        //売上データを読み込み、Saleオブジェクトへリストを返す
+        static List<Sale> ReadSales(string filePath) {
+            List<Sale> sales = new List<Sale>();
+
+
+
+            return sales;
+        }
     }
 }
