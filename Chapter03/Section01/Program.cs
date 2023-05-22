a19cb97c5986a4fefb95e83eb8ef7dbe0fb5bc20
櫻井 優奈 <OIBC34100@ojs.ac.jp>
2023-05-22 11:58:16 +0900

    ラムダ式以前(デリゲード、匿名メソッド)

--- Chapter03/Section01/Program.cs
+++ Chapter03/Section01/Program.cs
@@ -7,16 +7,18 @@ using System.Threading.Tasks;
 namespace Section01 {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine(Count(5));
-        }
-
-
-        static public int Count(int num) { 
             var numbers = new[] { 5, 3, 9, 6, 7, 5, 8, 1, 0, 5, 10, 4 };
+
+            int count = Count(numbers,delegate(int n) { return n % 2 == 0; });
+            Console.WriteLine(count);
+        }
+ 
+
+        static public int Count(int[] numbers, Predicate<int> judge) { 
             int count = 0;
 
-            for(int i = 0; i < numbers.Length; i++) {
-                if(numbers[i] == num) {
+            foreach(var n in numbers) {
+                if(judge(n) == true) {
                     count++;
                 }
             }
