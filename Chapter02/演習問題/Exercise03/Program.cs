0b414a22ebc0cc09b667a9cff9d9843168cf207b
櫻井 優奈 <OIBC34100@ojs.ac.jp>
2023-05-22 10:13:31 +0900

    演習2-3　インターフェース変更

--- Chapter02/演習問題/Exercise03/Program.cs
+++ Chapter02/演習問題/Exercise03/Program.cs
@@ -8,10 +8,31 @@ namespace Exercise03 {
     class Program {
         static void Main(string[] args) {
             var sales = new SalesCounter(@"data\sales.csv");
-            var amountPerCategory = sales.GetPerCategorySales();
-            foreach(var obj in amountPerCategory) {
-                Console.WriteLine("{0} {1:C}", obj.Key, obj.Value);
+
+            Console.WriteLine("＊＊売上集計＊＊");
+            Console.WriteLine("１．店舗別売上げ");
+            Console.WriteLine("２．商品カテゴリー別売上げ");
+
+            String num = Console.ReadLine();
+
+            switch(num) {
+                case "1":
+                    var amountPerStore = sales.GetPerStoreSales();
+                    foreach(var obj in amountPerStore) {
+                        Console.WriteLine("{0} {1:C}", obj.Key, obj.Value);
+                    }
+                    break;
+
+                case "2":
+                var amountPerCategory = sales.GetPerCategorySales();
+                foreach(var obj in amountPerCategory) {
+                    Console.WriteLine("{0} {1:C}", obj.Key, obj.Value);
+                }
+                    break;
             }
+
+
+            
         }
     }
 }
