86c53322a7ca685363df73861ef9008a94797da5
櫻井 優奈 <OIBC34100@ojs.ac.jp>
2023-05-24 10:53:21 +0900

    遅延実行確認

--- Chapter03/Section04/Program.cs
+++ Chapter03/Section04/Program.cs
@@ -11,17 +11,21 @@ namespace Section04 {
                "Tokyo", "New Delhi", "Bangkok", "London", "Paris", "Berlin", "Canberra", "Hong Kong",
             };
 
-
             //var query = names.Where(s => s.Length <= 5).Select(s => s.ToLower()); ;
 
-            var query = names.Select(s => s.Length);
-            foreach(var s in query) {
-                Console.WriteLine(s);
+            var query = names.Where(s => s.Length<=5).ToList();
+
+            foreach(var item  in query) {
+                Console.WriteLine(item);
             }
 
+            Console.WriteLine("------------");
 
+            names[0] = "Osaka";
 
-
+            foreach(var item in query) {
+                Console.WriteLine(item);
+            }
         }
     }
 }
