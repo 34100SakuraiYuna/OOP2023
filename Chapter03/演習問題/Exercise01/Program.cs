e6eb132dcffc69479b0e038babc63e35e784c111
櫻井 優奈 <OIBC34100@ojs.ac.jp>
2023-05-24 09:35:20 +0900

    演習３－２　完成

--- Chapter03/演習問題/Exercise01/Program.cs
+++ Chapter03/演習問題/Exercise01/Program.cs
@@ -35,6 +35,7 @@ namespace Exercise01 {
         }
 
         private static void Exercise1_2(List<int> numbers) {
+            numbers.ForEach(s=> Console.WriteLine(s/2.0)) ;
         }
 
         private static void Exercise1_3(List<int> numbers) {
