bf24a8b36458795d211b8d6c6ccf2409322fda99
櫻井 優奈 <OIBC34100@ojs.ac.jp>
2023-05-30 11:24:07 +0900

    Is12Centuryができた

---
+++ Chapter04/演習問題/Exercise01/Yearmonth.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    public class Yearmonth {
+        public Yearmonth(int year, int month) {
+            Year = year;
+            Month = month;
+        }
+
+        public int Year { get; private set; }
+        public int Month { get; private set; }
+
+
+
+
+        public bool Is12Century {
+            get {
+               return (2001 <= this.Year && this.Year <= 2100);
+            }
+        }
+
+        //public YearMonth addOneMonth() { 
+
+        //}
+
+
+        //public override string ToString() { 
+
+        //}
+    }
+}
---
+++ Chapter04/演習問題/Exercise02/Program.cs
@@ -0,0 +1,14 @@
+﻿using Exercise01;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02 {
+    class Program {
+        static void Main(string[] args) {
+            var ym = new YearMonth(2023,5);
+        }
+    }
+}
