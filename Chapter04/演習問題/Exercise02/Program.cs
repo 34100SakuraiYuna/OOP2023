715da3bc3c13e4fe7c9ef6b270b1a8a7723aa84a
櫻井 優奈 <OIBC34100@ojs.ac.jp>
2023-05-31 09:53:44 +0900

    4.2.2できた

--- Chapter04/演習問題/Exercise02/Program.cs
+++ Chapter04/演習問題/Exercise02/Program.cs
@@ -8,12 +8,44 @@ using System.Threading.Tasks;
 namespace Exercise02 {
     class Program {
         static void Main(string[] args) {
-            var ym = new Yearmonth(2023,5);
-            var c21 = ym.Is12Century;
-            var ymNextMonth = ym.addOneMonth();
+            // 4.2.1
+            var ymCollection = new Yearmonth[] {
+                new Yearmonth(1980, 1),
+                new Yearmonth(1990, 4),
+                new Yearmonth(2000, 7),
+                new Yearmonth(2010, 9),
+                new Yearmonth(2020, 12),
+            };
 
-            Console.WriteLine(ym);
-            Console.WriteLine(ymNextMonth);
+            // 4.2.2
+            Console.WriteLine("\n- 4.2.2 ---");
+            Exercise2_2(ymCollection);
+            Console.WriteLine("\n- 4.2.4 ---");
+
+            // 4.2.4
+            Exercise2_4(ymCollection);
+            Console.WriteLine("\n- 4.2.5 ---");
+
+
+            // 4.2.5
+            Exercise2_5(ymCollection);
+        }
+
+        private static void Exercise2_2(Yearmonth[] ymCollection) {
+            foreach(var ym in ymCollection) {
+                Console.WriteLine(ym);
+            }
+        }
+
+        //static Yearmonth FindFirst21C(Yearmonth[] yms) {
+           
+        //}
+
+        private static void Exercise2_4(Yearmonth[] ymCollection) {
+        }
+
+        private static void Exercise2_5(Yearmonth[] ymCollection) {
+         
         }
     }
 }
