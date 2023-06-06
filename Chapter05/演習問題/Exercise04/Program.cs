a137c8c6850f49cf9ebb066ceb411f4d4468ab7b
櫻井 優奈 <OIBC34100@ojs.ac.jp>
2023-06-06 10:14:34 +0900

    演習5.4　タイマー追加

--- Chapter05/演習問題/Exercise04/Program.cs
+++ Chapter05/演習問題/Exercise04/Program.cs
@@ -1,8 +1,9 @@
-﻿#define NonArray
-//#define StringArray
+﻿//#define NonArray
+#define StringArray
 
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,8 @@ using System.Threading.Tasks;
 namespace Exercise04 {
     class Program {
         static void Main(string[] args) {
+        Stopwatch sw = new Stopwatch();
+            sw.Start();
 
 #if NonArray
             var line = "Novelist=谷崎潤一郎;BestWork=春琴抄;Born=1886";
@@ -20,6 +23,9 @@ namespace Exercise04 {
             Console.WriteLine("誕生年：{0}", infos[(Array.IndexOf(infos, "Born")) + 1]);
 
 #elif StringArray
+            
+
+
             string[] lines = { "Novelist=谷崎潤一郎;BestWork=春琴抄;Born=1886",
                 "Novelist=夏目漱石;BestWork=坊ちゃん;Born=1867", "Novelist=太宰治;BestWork=人間失格;Born=1909",
                 "Novelist=宮沢賢治;BestWork=銀河鉄道の夜;Born=1896"};
@@ -29,8 +35,10 @@ namespace Exercise04 {
                 Console.WriteLine("作家：{0}", infos[(Array.IndexOf(infos, "Novelist")) + 1]);
                 Console.WriteLine("代表作：{0}", infos[(Array.IndexOf(infos, "BestWork")) + 1]);
                 Console.WriteLine("誕生年：{0}", infos[(Array.IndexOf(infos, "Born")) + 1]);
+           
             }
 #endif
+            Console.WriteLine("実行時間：{0}",sw.Elapsed.ToString(@"ss\.fffff"));
         }
     }
 }
