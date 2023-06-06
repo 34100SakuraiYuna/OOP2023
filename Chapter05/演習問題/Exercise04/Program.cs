df520b7d8866c0800dcef7d9fa0f2f49502920b9
櫻井 優奈 <OIBC34100@ojs.ac.jp>
2023-06-06 09:30:14 +0900

    演習問題5.4　1つ目できた

--- Chapter05/演習問題/Exercise04/Program.cs
+++ Chapter05/演習問題/Exercise04/Program.cs
@@ -7,7 +7,13 @@ using System.Threading.Tasks;
 namespace Exercise04 {
     class Program {
         static void Main(string[] args) {
-            var line = "NNovelist=谷崎潤一郎;BestWork=春琴;Born=1886";
+            var line = "NNovelist=谷崎潤一郎;BestWork=春琴抄;Born=1886";
+            var infos = line.Split(';','=').ToArray() ;
+
+            Console.WriteLine("作家：{0}",infos[1]);
+            Console.WriteLine("代表作：{0}", infos[3]);
+            Console.WriteLine("誕生年：{0}", infos[5]);
+
         }
     }
 }
