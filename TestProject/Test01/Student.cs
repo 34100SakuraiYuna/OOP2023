5ce4503eceb027e8ff0f5d1a77b45d78b2549d08
櫻井 優奈 <OIBC34100@ojs.ac.jp>
2023-06-19 14:54:18 +0900

    設問１_Student クラス完成

--- TestProject/Test01/Student.cs
+++ TestProject/Test01/Student.cs
@@ -1,7 +1,7 @@
 ﻿namespace Test01 {
     class Student {
-
-
-
+        public string Name { get; private set; }
+        public string Subject { get; private set; }
+        public int Score { get; private set; }
     }
 }
