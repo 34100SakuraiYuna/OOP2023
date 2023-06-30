2be8615e6a8e2567bfd9c9cc3013a6e42f92e313
櫻井 優奈 <OIBC34100@ojs.ac.jp>
2023-06-30 11:30:18 +0900

    演習8.1　できた

---
+++ Chapter08/演習問題/Exercise01/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    class Program {
+        static void Main(string[] args) {
+            var dateTime = new DateTime(2019, 1, 15, 19, 48, 32);
+            DisplayDatePattern1(dateTime);
+            DisplayDatePattern2(dateTime);
+            DisplayDatePattern3(dateTime);
+            DisplayDatePattern3_2(dateTime);
+        }
+
+        private static void DisplayDatePattern1(DateTime dateTime) {
+            var str = dateTime.ToString("yyyy/M/d HH:mm");
+
+            Console.WriteLine(str);
+        }
+
+        private static void DisplayDatePattern2(DateTime dateTime) {
+            var str2 = dateTime.ToString("yyyy年MM月dd日 HH時mm分ss秒");
+
+            Console.WriteLine(str2);
+        }
+
+        private static void DisplayDatePattern3(DateTime dateTime) {
+            var culture = new CultureInfo("ja-JP");
+            culture.DateTimeFormat.Calendar = new JapaneseCalendar();
+            var dayOfWeek = culture.DateTimeFormat.GetDayName(dateTime.DayOfWeek);
+            var str = dateTime.ToString("ggyy年 M月d日(" + dayOfWeek + ")", culture);
+
+            Console.WriteLine(str);
+        }
+
+        private static void DisplayDatePattern3_2(DateTime dateTime) {
+
+        }
+    }
+}
