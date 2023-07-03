d272194d0489490abd2825507899773e6dad64d3
櫻井 優奈 <OIBC34100@ojs.ac.jp>
2023-07-03 14:51:57 +0900

    演習8.3　スタートストップ追加

--- Chapter08/演習問題/Exercise03/Program.cs
+++ Chapter08/演習問題/Exercise03/Program.cs
@@ -9,18 +9,26 @@ namespace Exercise03 {
     class Program {
         static void Main(string[] args) {
             var tw = new TimeWatch();
+
+            Console.Write("スタート？");
+            Console.ReadLine();
             tw.Start();
-            Thread.Sleep(1000);
+
+            Console.Write("ストップ？");
+            Console.ReadLine();
             TimeSpan duration = tw.Stop();
-            Console.WriteLine("処理時間は{0}ミリ秒でした", duration.TotalMilliseconds);
+
+            Console.WriteLine("処理時間は{0}ミリ秒でした", duration.TotalSeconds);
         }
     }
 
     class TimeWatch {
         private DateTime _time;
+
         public void Start() {
             _time = DateTime.Now;
         }
+
 
         public TimeSpan Stop() {
             var duration = DateTime.Now - _time;
