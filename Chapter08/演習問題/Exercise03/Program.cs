9299816bbd8e827d33517b1d31c54c0831c83714
櫻井 優奈 <OIBC34100@ojs.ac.jp>
2023-07-03 13:55:00 +0900

    演習8.3　問題文を少し変更したためできた

---
+++ Chapter08/演習問題/Exercise03/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Exercise03 {
+    class Program {
+        static void Main(string[] args) {
+            var tw = new TimeWatch();
+            //tw.Start();
+            //Thread.Sleep(1000);
+            //TimeSpan duration = tw.Stop();
+            //Console.WriteLine("処理時間は{0}ミリ秒でした", duration.TotalMilliseconds);
+
+            var startTime = tw.Start();
+            Thread.Sleep(1000);
+            var duration = tw.Stop()- startTime;
+            Console.WriteLine("処理時間は{0}ミリ秒でした", duration.TotalMilliseconds);
+
+        }
+    }
+
+    class TimeWatch {
+        public DateTime Start() {
+            return DateTime.Now;
+        }
+
+        public DateTime Stop() {
+            return  DateTime.Now;
+        }
+    }
+}
