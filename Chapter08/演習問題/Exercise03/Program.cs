65b42c9a162b3c59cf4af909f19c5f9b547b485b
櫻井 優奈 <OIBC34100@ojs.ac.jp>
2023-07-03 14:29:34 +0900

    演習8.3　ちゃんとできた

--- Chapter08/演習問題/Exercise03/Program.cs
+++ Chapter08/演習問題/Exercise03/Program.cs
@@ -9,26 +9,22 @@ namespace Exercise03 {
     class Program {
         static void Main(string[] args) {
             var tw = new TimeWatch();
-            //tw.Start();
-            //Thread.Sleep(1000);
-            //TimeSpan duration = tw.Stop();
-            //Console.WriteLine("処理時間は{0}ミリ秒でした", duration.TotalMilliseconds);
-
-            var startTime = tw.Start();
+            tw.Start();
             Thread.Sleep(1000);
-            var duration = tw.Stop()- startTime;
+            TimeSpan duration = tw.Stop();
             Console.WriteLine("処理時間は{0}ミリ秒でした", duration.TotalMilliseconds);
-
         }
     }
 
     class TimeWatch {
-        public DateTime Start() {
-            return DateTime.Now;
+        private DateTime _time;
+        public void Start() {
+            _time = DateTime.Now;
         }
 
-        public DateTime Stop() {
-            return  DateTime.Now;
+        public TimeSpan Stop() {
+            var duration = DateTime.Now - _time;
+            return duration;
         }
     }
 }
