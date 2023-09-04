f4cf4cd6e20a062ae2ad5ed0adaabb07c96a7fd0
櫻井 優奈 <OIBC34100@ojs.ac.jp>
2023-09-04 15:59:27 +0900

    演習12.2.1と2　途中

---
+++ Chapter12/演習問題/Exercise02/Novelist.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Exercise02 {
+    [XmlRoot("novelist")]
+    [DataContract(Name = "novelist")]
+    public class Novelist {
+        [XmlElement(ElementName ="name")]
+        [DataMember(Name = "name")]
+        public string Name { get; set; }
+        [XmlElement(ElementName = "birth")]
+        [DataMember(Name = "birth")]
+        public DateTime Birth { get; set; }
+        [XmlElement(ElementName = "masterpieces")]
+        [DataMember(Name = "masterpieces")]
+        public string[] Masterpieces { get; set; }
+    }
+}
