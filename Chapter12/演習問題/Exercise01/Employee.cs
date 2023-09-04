4b0ec7fb07f44fd137caa454fcc79c8e1eb0ea18
櫻井 優奈 <OIBC34100@ojs.ac.jp>
2023-09-04 13:43:14 +0900

    演習12.1.1　完成

---
+++ Chapter12/演習問題/Exercise01/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Exercise01 {
+    [XmlRoot("employee")]
+    public class Employee {
+        [XmlElement(ElementName ="id")]
+        public int Id { get; set; }
+
+        [XmlElement(ElementName = "name")]
+        public string Name { get; set; }
+
+        [XmlElement(ElementName = "hiredate")]
+        public DateTime HireDate { get; set; }
+    }
+}
