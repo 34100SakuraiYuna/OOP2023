804f9b9355cca7c855fa06f509de30e03190e6f6
櫻井 優奈 <OIBC34100@ojs.ac.jp>
2023-09-26 11:35:23 +0900

    Section01　終了

---
+++ Chapter14/Section01/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿
+namespace Section01 {
+    partial class Form1 {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing) {
+            if(disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent() {
+            this.btRunNotepad = new System.Windows.Forms.Button();
+            this.btRunAndWaitNotepad = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btRunNotepad
+            // 
+            this.btRunNotepad.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.btRunNotepad.Font = new System.Drawing.Font("HG丸ｺﾞｼｯｸM-PRO", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btRunNotepad.Location = new System.Drawing.Point(36, 35);
+            this.btRunNotepad.Name = "btRunNotepad";
+            this.btRunNotepad.Size = new System.Drawing.Size(280, 127);
+            this.btRunNotepad.TabIndex = 0;
+            this.btRunNotepad.Text = "RunNotepad";
+            this.btRunNotepad.UseVisualStyleBackColor = false;
+            this.btRunNotepad.Click += new System.EventHandler(this.btRunNotepad_Click);
+            // 
+            // btRunAndWaitNotepad
+            // 
+            this.btRunAndWaitNotepad.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btRunAndWaitNotepad.Font = new System.Drawing.Font("HG丸ｺﾞｼｯｸM-PRO", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btRunAndWaitNotepad.Location = new System.Drawing.Point(405, 38);
+            this.btRunAndWaitNotepad.Name = "btRunAndWaitNotepad";
+            this.btRunAndWaitNotepad.Size = new System.Drawing.Size(280, 127);
+            this.btRunAndWaitNotepad.TabIndex = 1;
+            this.btRunAndWaitNotepad.Text = "RunAndWaitNotepad";
+            this.btRunAndWaitNotepad.UseVisualStyleBackColor = false;
+            this.btRunAndWaitNotepad.Click += new System.EventHandler(this.btRunAndWaitNotepad_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btRunAndWaitNotepad);
+            this.Controls.Add(this.btRunNotepad);
+            this.Name = "Form1";
+            this.Text = "14章 1節";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btRunNotepad;
+        private System.Windows.Forms.Button btRunAndWaitNotepad;
+    }
+}
+
