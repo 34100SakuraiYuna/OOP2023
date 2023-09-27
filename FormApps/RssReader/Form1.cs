449f8bb0d5b4b85974863e20d95ce532d7551377
櫻井 優奈 <OIBC34100@ojs.ac.jp>
2023-09-27 11:29:46 +0900

    RSS　タイトルの表示

--- FormApps/RssReader/Form1.cs
+++ FormApps/RssReader/Form1.cs
@@ -4,14 +4,30 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Linq;
 
 namespace RssReader {
     public partial class Form1 : Form {
         public Form1() {
             InitializeComponent();
+        }
+
+        private void btGet_Click(object sender, EventArgs e) {
+            using(var wc = new WebClient()) {
+                var url = wc.OpenRead(tbUrl.Text);
+                XDocument xdoc = XDocument.Load(url) ;
+
+                var nodes = xdoc.Root.Descendants("title");
+                foreach(var node in nodes) {
+                    string s = Regex.Replace(node.Value, "【|】", "");
+                    lbRssTitle.Items.Add(s);
+                }
+            }
         }
     }
 }
