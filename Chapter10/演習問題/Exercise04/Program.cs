2aa613726b46ad53e62200166843e98c0e183072
櫻井 優奈 <OIBC34100@ojs.ac.jp>
2023-10-05 12:05:07 +0900

    演習10.4　力技でできた

--- Chapter10/演習問題/Exercise04/Program.cs
+++ Chapter10/演習問題/Exercise04/Program.cs
@@ -3,17 +3,21 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Exercise04 {
     class Program {
         static void Main(string[] args) {
+            var count = -1;
             var lines = File.ReadAllLines("sample.txt");
+            foreach(var item in lines) {
+                count ++;
+                lines[count] = Regex.Replace(item,@".*(=).*","=");
+                lines[count] = Regex.Replace(item, "v4.0", "v5.0");
+            }
 
-
-
-
-
+            File.WriteAllLines("sample.txt",lines);
 
             // これ以降は確認用
             var text = File.ReadAllText("sample.txt");
