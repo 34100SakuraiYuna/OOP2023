7144c838176eb65d331242900193dd62d01e56d0
櫻井 優奈 <OIBC34100@ojs.ac.jp>
2023-10-10 09:58:58 +0900

    演習10.4　模範解答追加

--- Chapter10/演習問題/Exercise04/Program.cs
+++ Chapter10/演習問題/Exercise04/Program.cs
@@ -9,16 +9,23 @@ using System.Threading.Tasks;
 namespace Exercise04 {
     class Program {
         static void Main(string[] args) {
-            var count = -1;
             var lines = File.ReadAllLines("sample.txt");
-            foreach(var item in lines) {
-                count ++;
-                lines[count] = Regex.Replace(item,@".*(=).*","=");
-                lines[count] = Regex.Replace(item, "v4.0", "v5.0");
-            }
 
-            File.WriteAllLines("sample.txt",lines);
+            #region　自分の回答
+            //var count = -1;
 
+            //foreach(var item in lines) {
+            //    count ++;
+            //    lines[count] = Regex.Replace(item,@".*(=).*","=");
+            //    lines[count] = Regex.Replace(item, "v4.0", "v5.0");
+            //}
+            //File.WriteAllLines("sample.txt", lines);
+            #endregion
+
+            //模範解答
+            var newLines = lines.Select(s=> Regex.Replace(s,@"\b(V|v)ersion\s*=\s*""v4.0""",@"version=""5.0"""));
+            File.WriteAllLines("sample.txt",newLines);
+            
             // これ以降は確認用
             var text = File.ReadAllText("sample.txt");
             Console.WriteLine(text);
