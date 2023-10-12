c7ba093b3319695d135354006c7e59a1c1a8fd6f
櫻井 優奈 <OIBC34100@ojs.ac.jp>
2023-10-12 11:56:36 +0900

    演習15.1.8　完成 演習15.1.3・4・5　模範解答追加

--- Chapter15/演習問題/Exercise01/Program.cs
+++ Chapter15/演習問題/Exercise01/Program.cs
@@ -49,8 +49,16 @@ namespace Exercise01 {
                 foreach(var book in g) {
                     count++;
                 }
-                Console.WriteLine("　{0}冊",count);
+                Console.WriteLine("　{0}冊", count);
             }
+
+            //模範解答
+            //var query = Library.Books.GroupBy(b => b.PublishedYear)
+            //    .Select(g => new { PublishedYear = g.Key, Count = g.Count() })
+            //    .OrderBy(x => x.PublishedYear);
+            //foreach(var item in query) {
+            //    Console.WriteLine("{0}年　{1}冊", item.PublishedYear, item.Count);
+            //}
         }
 
 
@@ -59,19 +67,51 @@ namespace Exercise01 {
             var categorys = Library.Categories.ToList();
             foreach(var g in groups) {
                 Console.WriteLine("{0}年　{1}円　{2}　({3})"
-                    ,g.PublishedYear,g.Price,g.Title,categorys[(g.CategoryId)-1].Name);
+                    , g.PublishedYear, g.Price, g.Title, categorys[(g.CategoryId) - 1].Name);
             }
+
+            //模範解答
+            //var query = Library.Books.Join(Library.Categories,
+            //    book=> book.CategoryId,
+            //    category=> category.Id,
+            //    (book,category)=> new {
+            //            PublishedYear = book.PublishedYear,
+            //            Price = book.Price,
+            //            Title = book.Title,
+            //            CategoryName = category.Name,
+            //        }
+            //    ).OrderByDescending(x=> x.PublishedYear)
+            //    .ThenByDescending(x=> x.Price);
+
+            //foreach(var item in query) {
+            //    Console.WriteLine("{0}年　{1}円　{2}　({3})"
+            //        ,item.PublishedYear,item.Price,item.Title,item.CategoryName);
+            //}
+
         }
 
 
         private static void Exercise1_5() {
             var lookup = Library.Books.ToLookup(b => b.PublishedYear);
             var books = lookup[2016];
-            var categorys = Library.Categories.Select(c=> c.Name).ToList();
+            var categorys = Library.Categories.Select(c => c.Name);
 
             foreach(var item in categorys) {
                 Console.WriteLine(item);
             }
+
+
+            //模範解答
+            //var query = Library.Books.Where(b=> b.PublishedYear == 2016)
+            //    .Join(Library.Categories,
+            //    book => book.CategoryId,
+            //    category => category.Id,
+            //    (book, category) => category.Name).Distinct();
+
+            //foreach(var name in query) {
+            //    Console.WriteLine(name);
+            //}
+
         }
 
 
@@ -93,10 +133,37 @@ namespace Exercise01 {
 
 
         private static void Exercise1_7() {
+            var books = Library.Books.GroupBy(b=> b.PublishedYear).OrderBy(b=> b.Key);
+
+            foreach(var item in books) {
+                Console.WriteLine("#{0}年",item.Key);
+
+                foreach(var i in item) {
+                    var a =  Library.Categories.Where(c=> c.Name == "Development").Select(c=> c.Id);
+
+                    if(a.Equals(i.CategoryId)) {
+                        Console.WriteLine("　{0}",i.Title);
+
+                    }
+                }
+            }
         }
 
 
         private static void Exercise1_8() {
+            var groups = Library.Categories.GroupJoin(Library.Books,
+                c => c.Id,
+                b => b.CategoryId,
+                (c, books) => new {
+                    Category = c.Name,
+                    Books = books
+                });
+
+            foreach(var group in groups) {
+                if(group.Books.Count() >= 4) {
+                    Console.WriteLine(group.Category);
+                }
+            }
         }
     }
 }
